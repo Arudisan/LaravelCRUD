9e969e2fa7bbf62c4bc49db4e6e5b763c3cfdfd2
Ardi <ardibbmme@gmail.com>
2022-09-21 18:56:45 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+   public function home() {
+    return view('pages.home');
+   }
+   public function about() {
+    return view('pages.about');
+   }
+    public function contact() {
+    return view('pages.contact');
+    }
+    public function pricing() {
+    return view('pages.pricing');
+    }
+    public function worksingle() {
+        return view('pages.worksingle');
+    }
+    public function work() {
+    return view('pages.work');
+     }
+
+}
