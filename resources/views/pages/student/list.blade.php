f8c9d2ca32062e4f1dad140cd01ac5d5cc9dff48
Ardi <ardibbmme@gmail.com>
2022-10-05 20:12:05 +0700

    setup pagination

--- resources/views/pages/student/list.blade.php
+++ resources/views/pages/student/list.blade.php
@@ -1,41 +1,42 @@
 @extends('layouts.dashboard')
 @section('content')
-<h1>Hello, world!</h1>
-@if($message = Session::get('notif'))
-<div class="alert alert-primary" role="alert">
-    <strong>{{ $message }}</strong>
-  </div>
-  @endif
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Name</th>
-        <th scope="col">Date Birth</th>
-        <th scope="col">Gender</th>
-        <th scope="col">Address</th>
-        <th scope="col">Major</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ( $data as $item )
-      <tr>
-        <th scope="row">{{ $loop->iteration }}</th>
-        <td>{{ $item->name}}</td>
-        <td>{{ $item->date_birth}}</td>
-        <td>{{ $item->gender}}</td>
-        <td>{{ $item->address}}</td>
-        <td>{{ $item->major->name}}</td>
-        <td>
-            <a href="{{route('student.edit',['student'=>$item->id]) }}" class="btn btn-primary">edit</a>
-            <form action="{{ route('student.destroy',['student'=> $item->id]) }}" method="post">
-                @method('delete')
-                @csrf
-                <button type="submit" class="btn btn-danger">Delete</button>
-            </form>
-        </td>
-      </tr>
-      @endforeach
-  </table>
+    <h1>Hello, world!</h1>
+    @if ($message = Session::get('notif'))
+        <div class="alert alert-primary" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+    @endif
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Date Birth</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Address</th>
+                <th scope="col">Major</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $item)
+                <tr>
+                    <th scope="row">{{ ($data->currentPage() - 1) * $data->perPage() + $loop->iteration }}</th>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->date_birth }}</td>
+                    <td>{{ $item->gender }}</td>
+                    <td>{{ $item->address }}</td>
+                    <td>{{ $item->major->name }}</td>
+                    <td>
+                        <a href="{{ route('student.edit', ['student' => $item->id]) }}" class="btn btn-primary">edit</a>
+                        <form action="{{ route('student.destroy', ['student' => $item->id]) }}" method="post">
+                            @method('delete')
+                            @csrf
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+    </table>
+    {{ $data->links() }}
 @endsection
